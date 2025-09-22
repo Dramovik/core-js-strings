02f5912de0b488a7cd0dbba66c8a44fc441efd5f
Dramovik <dramovik1@gmail.com>
2025-09-22 19:54:10 +0300

    feat: reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -376,8 +376,13 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((item) => {
+      return item.split('').reverse().join('');
+    })
+    .join(' ');
 }
 
 /**
